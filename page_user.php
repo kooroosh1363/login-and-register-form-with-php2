f541ca5a681cf300d22112dda8e95915fa8fbc15
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-19 23:30:14 +0400

    add css code

--- page_user.php
+++ page_user.php
@@ -4,21 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/style.css echo time(); ?>">
+    <link rel="stylesheet" href="./assets/style.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <title>Page-User</title>
-    
+
 </head>
 
 <body>
     <div class="container">
-        <h3>Hello , <span>user</span></h3>
-        <h1>Welcome <span></span></h1>
-        <p>This Is An User Page</p>
-        <a href="./login.php">Login</a>
-        <a href="./register.php">Register</a>
-        <a href="./logout.php">Logout</a>
+        <div class="content">
+            <h3>Hello , <span>user</span></h3>
+            <h1>Welcome <span></span></h1>
+            <p>This Is An User Page</p>
+            <a href="./login.php">Login</a>
+            <a href="./register.php">Register</a>
+            <a href="./logout.php">Logout</a>
+        </div>
     </div>
 
 
